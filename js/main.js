89177604e5ef085232c23cc2b69e3918418b4a28
TAHAWE <amer.zp582@gmail.com>
2021-12-08 22:31:11 +0200

    Add Digital Clock

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ let hr = document.querySelector(".hr");
 let mn = document.querySelector(".mn");
 let sc = document.querySelector(".sc");
 
+// Digital Clock
+let analog_time = document.querySelector(".clock");
+let time = document.querySelector(".digital-clock");
+
+analog_time.onclick = () => {
+  time.style.display = "block";
+  analog_time.style.display = "none";
+};
+time.onclick = () => {
+  time.style.display = "none";
+  analog_time.style.display = "inherit";
+};
+
 setInterval(() => {
   let dey = new Date();
 
@@ -14,7 +27,37 @@ setInterval(() => {
   hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
+
+  // Digital Clock
+  let hours = dey.getHours();
+  let minutes = dey.getMinutes();
+  let seconds = dey.getSeconds();
+
+  // hour.innerHTML = `${hhh - 12}` < 10 ? `0${hhh - 12}` : `${hhh - 12}`;
+  // minutes.innerHTML = `${mmm}` < 10 ? `0${mmm}` : `${mmm}`;
+  // seconds.innerHTML = `${sss}` < 10 ? `0${sss}` : `${sss}`;
+
+  // Day Night AM & PM
+  let dayNight = "AM";
+  if (hours > 12) {
+    dayNight = "PM";
+    hours = hours - 12;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  time.innerHTML = `${hours} : ${minutes} : ${seconds} : ${dayNight}`;
+  day_night.innerHTML = dayNight;
 });
+
+// Toggle Analog Clock And Digital Clock
 
 // Toggle Code
 
